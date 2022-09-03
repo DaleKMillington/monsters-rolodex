8eba92a0c3c0ceea8c4bc58b83b8011fcf71996e
Dale Millington <dkmillington@tutanota.com>
2022-09-03 20:46:40 +0100

    Setup API fetch for users

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { Component } from 'react'
 
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -8,22 +7,33 @@ class App extends Component {
     super();
 
     this.state = {
-      name: 'Dale'
+      monsters: []
     };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => {
+        this.setState(
+          () => {
+            return {monsters: users};
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      })
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Hi {this.state.name}</p>
-          <button onClick={() => {
-            this.setState({name: 'Bex'});
-          }}>
-            Change Name
-          </button>
-        </header>
+        {
+          this.state.monsters.map(monster => {
+            return <h1 key={monster.name}>{monster.name}</h1>
+          })
+        }
       </div>
     );
   }
