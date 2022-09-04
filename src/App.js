a82df10a6b8823c77271d834e072ef63d9cc7ae8
Dale Millington <dkmillington@tutanota.com>
2022-09-04 15:17:48 +0100

    Changed indentation to 4 spaces

--- src/App.js
+++ src/App.js
@@ -1,42 +1,40 @@
 import { Component } from 'react'
-
 import './App.css';
 
 class App extends Component {
-  constructor() {
-    super();
+    constructor() {
+        super();
+        this.state = {
+            monsters: []
+        };
+    }
 
-    this.state = {
-      monsters: []
-    };
-  }
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => {
+                this.setState(
+                    () => {
+                        return {monsters: users};
+                    },
+                    () => {
+                        console.log(this.state);
+                    }
+                )
+            })
+    }
 
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => {
-        this.setState(
-          () => {
-            return {monsters: users};
-          },
-          () => {
-            console.log(this.state);
-          }
-        )
-      })
-  }
-
-  render() {
-    return (
-      <div className="App">
-        {
-          this.state.monsters.map(monster => {
-            return <h1 key={monster.name}>{monster.name}</h1>
-          })
-        }
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                {
+                    this.state.monsters.map(monster => {
+                        return <h1 key={monster.name}>{monster.name}</h1>
+                    })
+                }
+            </div>
+        );
+    }
 }
 
 export default App;
